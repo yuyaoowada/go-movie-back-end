15db2bb7bddd3822f0d3ec18ad527776e1f7f052
yuyaoowada <yuyaoowada@gmail.com>
2023-09-24 00:15:00 +0900

    modify URL

---
+++ movies.sql
@@ -0,0 +1,301 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.5 (Debian 14.5-2.pgdg110+2)
+-- Dumped by pg_dump version 16.0
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: genres; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.genres (
+    id integer NOT NULL,
+    genre character varying(255),
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+--
+-- Name: genres_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+ALTER TABLE public.genres ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.genres_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: movies; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.movies (
+    id integer NOT NULL,
+    title character varying(512),
+    release_date date,
+    runtime integer,
+    mpaa_rating character varying(10),
+    description text,
+    image character varying(255),
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+--
+-- Name: movies_genres; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.movies_genres (
+    id integer NOT NULL,
+    movie_id integer,
+    genre_id integer
+);
+
+
+--
+-- Name: movies_genres_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+ALTER TABLE public.movies_genres ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.movies_genres_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: movies_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+ALTER TABLE public.movies ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.movies_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    first_name character varying(255),
+    last_name character varying(255),
+    email character varying(255),
+    password character varying(255),
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+ALTER TABLE public.users ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Data for Name: genres; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.genres (id, genre, created_at, updated_at) FROM stdin;
+1	Comedy	2022-09-23 00:00:00	2022-09-23 00:00:00
+2	Sci-Fi	2022-09-23 00:00:00	2022-09-23 00:00:00
+3	Horror	2022-09-23 00:00:00	2022-09-23 00:00:00
+4	Romance	2022-09-23 00:00:00	2022-09-23 00:00:00
+5	Action	2022-09-23 00:00:00	2022-09-23 00:00:00
+6	Thriller	2022-09-23 00:00:00	2022-09-23 00:00:00
+7	Drama	2022-09-23 00:00:00	2022-09-23 00:00:00
+8	Mystery	2022-09-23 00:00:00	2022-09-23 00:00:00
+9	Crime	2022-09-23 00:00:00	2022-09-23 00:00:00
+10	Animation	2022-09-23 00:00:00	2022-09-23 00:00:00
+11	Adventure	2022-09-23 00:00:00	2022-09-23 00:00:00
+12	Fantasy	2022-09-23 00:00:00	2022-09-23 00:00:00
+13	Superhero	2022-09-23 00:00:00	2022-09-23 00:00:00
+\.
+
+
+--
+-- Data for Name: movies; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.movies (id, title, release_date, runtime, mpaa_rating, description, image, created_at, updated_at) FROM stdin;
+1	Highlander	1986-03-07	116	R	He fought his first battle on the Scottish Highlands in 1536. He will fight his greatest battle on the streets of New York City in 1986. His name is Connor MacLeod. He is immortal.	/8Z8dptJEypuLoOQro1WugD855YE.jpg	2022-09-23 00:00:00	2022-09-23 00:00:00
+2	Raiders of the Lost Ark	1981-06-12	115	PG-13	Archaeology professor Indiana Jones ventures to seize a biblical artefact known as the Ark of the Covenant. While doing so, he puts up a fight against Renee and a troop of Nazis.	/ceG9VzoRAVGwivFU403Wc3AHRys.jpg	2022-09-23 00:00:00	2022-09-23 00:00:00
+3	The Godfather	1972-03-24	175	18A	The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.	/3bhkrj58Vtu7enYsRolD1fZdja1.jpg	2022-09-23 00:00:00	2022-09-23 00:00:00
+4	sergh	2023-09-03	132	R	serg	/vEYr1sJR1dOFGXwXawpBN6hDRGF.jpg	2023-09-18 11:26:16.951789	2023-09-18 11:26:16.95179
+5	yhrgb	2023-09-06	12345	PG	dfgh		2023-09-18 11:27:33.170805	2023-09-18 11:27:33.170806
+6	fgbvb	2023-09-08	43	R	bgf		2023-09-18 11:30:16.437948	2023-09-18 11:30:16.437948
+7	er	2023-09-12	54	R	ftyhj	/fTUCwsxVoLFuodAtlL6aITP1B45.jpg	2023-09-18 11:31:01.560306	2023-09-18 11:31:01.560306
+8	dfg	2023-09-08	45	R	fg		2023-09-18 11:35:42.760842	2023-09-18 11:35:42.760843
+9	derty	2023-09-02	65	R	fg		2023-09-18 11:39:46.036412	2023-09-18 11:39:46.036412
+10	345	2023-09-05	345	R	fty	/usNwfeKc2DMJmKLmPxQeZo1ry6z.jpg	2023-09-18 12:00:44.285982	2023-09-18 12:00:44.285982
+11	Die Hard	1988-07-20	132	R	rgrgsrgggr....	/yFihWxQcmqcaBR31QM6Y8gT6aYV.jpg	2023-09-18 12:10:56.835545	2023-09-18 18:05:51.190404
+\.
+
+
+--
+-- Data for Name: movies_genres; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.movies_genres (id, movie_id, genre_id) FROM stdin;
+1	1	5
+2	1	12
+3	2	5
+4	2	11
+5	3	9
+6	3	7
+7	4	5
+8	5	5
+9	5	10
+10	6	10
+11	7	5
+12	8	5
+13	9	11
+14	10	5
+51	11	5
+52	11	9
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.users (id, first_name, last_name, email, password, created_at, updated_at) FROM stdin;
+1	Admin	User	admin@example.com	$2a$14$wVsaPvJnJJsomWArouWCtusem6S/.Gauq/GjOIEHpyh2DAMmso1wy	2022-09-23 00:00:00	2022-09-23 00:00:00
+\.
+
+
+--
+-- Name: genres_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.genres_id_seq', 13, true);
+
+
+--
+-- Name: movies_genres_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.movies_genres_id_seq', 54, true);
+
+
+--
+-- Name: movies_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.movies_id_seq', 44, true);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+
+
+--
+-- Name: genres genres_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.genres
+    ADD CONSTRAINT genres_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: movies_genres movies_genres_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.movies_genres
+    ADD CONSTRAINT movies_genres_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: movies movies_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.movies
+    ADD CONSTRAINT movies_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: movies_genres movies_genres_genre_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.movies_genres
+    ADD CONSTRAINT movies_genres_genre_id_fkey FOREIGN KEY (genre_id) REFERENCES public.genres(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: movies_genres movies_genres_movie_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.movies_genres
+    ADD CONSTRAINT movies_genres_movie_id_fkey FOREIGN KEY (movie_id) REFERENCES public.movies(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: -
+--
+
+REVOKE USAGE ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
